016922b08746101a03c05db0398e5aca5dcf678b
HyperGeek <hypergeek@teamgeekout.com>
2025-12-02 14:44:30 -0600

    Reorganized day 2 into a cursed chain of iterator functions because I'm a psychopath and rust enables me.

--- rust/src/day02/main.rs
+++ rust/src/day02/main.rs
@@ -5,32 +5,28 @@ use fancy_regex::Regex;
 
 fn part1(line: String) -> Result<u64> {
     let re = Regex::new(r"^([0-9]+)\1$")?;
-    let mut invalid_sum = 0;
 
-    for sub_input in line.split(',') {
-        let (first, last) = sub_input.split_once('-').unwrap();
-
-        let this_range = (first.parse::<u64>()?)..(last.parse::<u64>()? + 1);
-        invalid_sum += this_range
-            .filter(|x| re.is_match(&x.to_string()).unwrap())
-            .sum::<u64>();
-    }
+    let invalid_sum = line
+        .split(',')
+        .map(|x| x.split_once('-').unwrap())
+        .map(|(first, last)| (first.parse::<u64>().unwrap())..(last.parse::<u64>().unwrap() + 1))
+        .flatten()
+        .filter(|x| re.is_match(&x.to_string()).unwrap())
+        .sum::<u64>();
 
     Ok(invalid_sum)
 }
 
 fn part2(line: String) -> Result<u64> {
     let re = Regex::new(r"^([0-9]+)\1+$")?;
-    let mut invalid_sum = 0;
 
-    for sub_input in line.split(',') {
-        let (first, last) = sub_input.split_once('-').unwrap();
-
-        let this_range = (first.parse::<u64>()?)..(last.parse::<u64>()? + 1);
-        invalid_sum += this_range
-            .filter(|x| re.is_match(&x.to_string()).unwrap())
-            .sum::<u64>();
-    }
+    let invalid_sum = line
+        .split(',')
+        .map(|x| x.split_once('-').unwrap())
+        .map(|(first, last)| (first.parse::<u64>().unwrap())..(last.parse::<u64>().unwrap() + 1))
+        .flatten()
+        .filter(|x| re.is_match(&x.to_string()).unwrap())
+        .sum::<u64>();
 
     Ok(invalid_sum)
 }
