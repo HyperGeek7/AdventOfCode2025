8c1ac20604d3dece0a34c4e4738347a91fa25f57
HyperGeek <hypergeek@teamgeekout.com>
2025-12-04 14:50:35 -0600

    Refactored to remove duplicate code.

--- rust/src/day04.rs
+++ rust/src/day04.rs
@@ -8,20 +8,16 @@ fn prep_map(lines: &Vec<&str>) -> HashMap<(i32, i32), char> {
 
     for (y, line) in lines.iter().enumerate() {
         for (x, char) in line.char_indices() {
-            // This could really *just* contain the locations of the
-            // "@" characters to save some time and memory,
-            // but I didn't realize that until I was basically done.
-            // Alas.
-            floor_map.insert((x as i32, y as i32), char);
+            if char == '@' {
+                floor_map.insert((x as i32, y as i32), char);
+            }
         }
     }
 
     floor_map
 }
 
-fn part1(input_lines: &Vec<&str>) -> Result<usize> {
-    let floor_map = prep_map(input_lines);
-
+fn find_removables(floor_map: &HashMap<(i32, i32), char>) -> Vec<(i32, i32, usize)> {
     // Oh look, another functional programming hellscape!
     // ...Listen, I just find these fun to write. They're definitely
     // not the *best* way to handle everything.
@@ -31,13 +27,16 @@ fn part1(input_lines: &Vec<&str>) -> Result<usize> {
     // (how many rolls each roll is touching), and then count how many elements of *that*
     // grid are less than 4. So, easy enough to do.
 
-    let result = floor_map
+    floor_map
         // Iterate over all the key-value pairs
         .iter()
         // Discard any spaces that aren't occupied
         .filter(|((_, _), char)| **char == '@')
         // Count how many rolls each roll is touching, which is its own (somewhat involved) function.
-        .map(|((x, y), _)| {
+        .map(|((x, y), _)| { 
+            (
+            *x,
+            *y,
             // Get the coordinates of all the surrounding "tiles" by way of the cartesian product.
             iproduct!((x - 1)..(x + 2), (y - 1)..(y + 2))
                 // The cartesian product also includes the middle tile itself, so discard that.
@@ -51,17 +50,22 @@ fn part1(input_lines: &Vec<&str>) -> Result<usize> {
                 })
                 // And count how many we have left
                 .count()
+            )
         })
         // So that terrifying map() call has transformed our original grid of characters into a
         // sequence of numbers that indicates how many rolls each tile is touching.
         // We've lost the actual grid component along the way, but that doesn't matter for
         // this particular puzzle.
         // That done, we just discard any tile with more than 3 occupied neighbors...
-        .filter(|occupied_slots| *occupied_slots < 4)
-        // And then count how many elements we have left.
-        .count();
+        .filter(|(_, _, occupied_slots)| *occupied_slots < 4)
+        .collect()
 
-    // See? Nothing simpler!
+}
+
+fn part1(input_lines: &Vec<&str>) -> Result<usize> {
+    let floor_map = prep_map(input_lines);
+
+    let result = find_removables(&floor_map).len();
 
     Ok(result)
 }
@@ -70,37 +74,8 @@ fn part2(input_lines: &Vec<&str>) -> Result<usize> {
     let mut floor_map = prep_map(input_lines);
     let mut result = 0;
 
-    // This is basically just part 1 in a loop, so see that function for
-    // how all the heavy lifting works.
-
-    // The key difference this time is in what we're returning from
-    // the functional programming hellscape: instead of just the count,
-    // we're also getting back the coordinates of the point.
-    // Once we note how many of these points there are, we also
-    // remove them from the map, then go back and run the process again.
-    // Once we have a run where nothing comes back, we're done.
-
     loop {
-        let these_moves: Vec<(i32, i32, usize)> = floor_map
-            .iter()
-            .filter(|((_, _), char)| **char == '@')
-            .map(|((x, y), _)| {
-                (
-                    *x,
-                    *y,
-                    iproduct!((x - 1)..(x + 2), (y - 1)..(y + 2))
-                        .filter(|(offset_x, offset_y)| offset_x != x || offset_y != y)
-                        .filter_map(|(offset_x, offset_y)| {
-                            match floor_map.get(&(offset_x, offset_y)).unwrap_or(&'.') {
-                                '@' => Some(1),
-                                _ => None,
-                            }
-                        })
-                        .count(),
-                )
-            })
-            .filter(|(_, _, occupied_slots)| *occupied_slots < 4)
-            .collect();
+        let these_moves: Vec<(i32, i32, usize)> = find_removables(&floor_map);
 
         if these_moves.is_empty() {
             break;
